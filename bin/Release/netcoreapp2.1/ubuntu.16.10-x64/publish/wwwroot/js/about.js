352add3e8d6494be2fc3cdca1d31eba19853ce3d
Xrosby <mads.sfg@gmail.com>
2018-11-13 22:12:34 +0100

    os til ubunto

---
+++ bin/Release/netcoreapp2.1/ubuntu.16.10-x64/publish/wwwroot/js/about.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    console.log("Works")
+
+    $('#send-button').on('click', function () {
+
+
+        let name = $('#input-name').val();
+        let number = $('#input-number').val();
+        let streetname = $('#input-street-name').val();
+
+        let message = name + ' vil gerne gøres hård. Ring til ham på ' + number + ', eller kom til ' + streetname;
+
+        let url = 'https://platform.clickatell.com/messages/http/send?apiKey=Me8n5IHUSh6Az66fjifvhg==&to=4551949609&content=' + message;
+
+        $.ajax({
+            url: url,
+            success: function () {
+                alert('Beskeden blev sendt. Lasse kontakter dig hurtigst muligt!');
+            }
+        });
+
+    })
+
+
+
+})
